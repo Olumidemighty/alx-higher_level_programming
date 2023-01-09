e7dc4ea807af49e3cf7e9abf67dda7dc8b62bdb2
Fesobi Olumide Phillip <fesobiolumide@gmail.com>
2023-01-09 12:31:34 -0800

    new

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""MyList module"""
+
+
+class MyList(list):
+    """MyList class - Inherits from list"""
+    def print_sorted(self):
+        """Prints a sorted list"""
+        print(sorted(self))
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+
